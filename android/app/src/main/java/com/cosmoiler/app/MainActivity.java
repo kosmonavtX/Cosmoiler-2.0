25c021b751c8b3ffb5c3c90968c5a8642041da10
kosmonavtX <jawaman76@gmail.com>
2021-04-20 12:12:07 +0300

    Исправление ошибок

--- android/app/src/main/java/com/cosmoiler/app/MainActivity.java
+++ android/app/src/main/java/com/cosmoiler/app/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import android.view.WindowManager;
+
 
 import java.util.ArrayList;
 
@@ -11,11 +13,16 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // экран всегда включен!
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+  }
+
+  @Override
+  public void onBackPressed() {
+    finish();
   }
 }
