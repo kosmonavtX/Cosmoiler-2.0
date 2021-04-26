6a8cc44d37f40168f43da6399bd2ffcd8f53714c
cosmoiler <cosmoiler@gmail.com>
2021-04-26 17:18:52 +0300

    More page develop

--- src/pages/more/system.svelte
+++ src/pages/more/system.svelte
@@ -5,21 +5,72 @@
 
   <Navbar title={$t('home.settings')} backLink="Back" />
 
-  <Block strong>
-    <p>1. Яркость светодиода</p>
-    <p>2. Восстановление заводских настроек</p>
-    <p>3. Прокачка</p>
-  </Block>
-
+  <BlockTitle >
+    <span>Прокачка</span>
+  </BlockTitle>
   <List >
     <ListItem>
-      <div slot='title' class="list-input__label-text_color">Прокачка системы</div>
-      <span slot="after">
-        <Toggle bind:checked={system.ap.pwr}  /> <!-- on:toggleChange={() => log(system.ap.pwr)} -->
-      </span>
+      <ListItemCell class="width-auto flex-shrink-0 list-input__label-text_color">Заполнение системы маслом:</ListItemCell>
+      <ListItemCell class="width-auto flex-shrink-4"><Toggle bind:checked={system.ap.pwr}  /></ListItemCell>
+      <!-- <div slot='title' class="list-input__label-text_color">Прокачка системы</div> -->
+<!--       <span slot="after">
+        <Toggle bind:checked={system.ap.pwr}  />
+      </span> -->
     </ListItem>
   </List>
 
+  <BlockTitle >
+    <span>Заводские настройки</span>
+  </BlockTitle>
+
+  <List mediaList class='elevation-0'>
+    <ListItem>
+      <ListItemCell class="width-auto flex-shrink-0 list-input__label-text_color"><Icon icon="icon-info-circled" style="font-size: 28px; color: orange" /></ListItemCell>
+      <ListItemCell class="width-auto flex-shrink-3 list-input__label-text_color"><Button fill small onClick={() => log("Button")}>
+        Сброс
+      </Button></ListItemCell>
+      <!-- <ListItemCell class="width-auto flex-shrink-0 list-input__label-text_color">Заполнение системы маслом:</ListItemCell> -->
+
+<!--       <div slot='title' class="list-input__label-text_color">Внимание! Сброс приведет к установлению заводских настроек</div> -->
+<!--       <div slot='text' class="list-input__label-text_color">      <Button fill small onClick={() => log("Button")}>
+        Сброс
+      </Button></div> -->
+      <!-- <img slot="media" src="static/img/icon.png" width="32" /> -->
+<!--       <div slot='media'>
+        <Icon icon="icon-info-circled" style="font-size: 28px; color: orange" />
+      </div> -->
+
+    </ListItem>
+  </List>
+  <List mediaList class='elevation-0'>
+    <ListItem>
+      <div slot='media'>
+        <Icon icon="icon-info-circled" style="font-size: 28px; color: orange" />
+      </div>
+      <div slot='after'>
+        <Button fill small click={clickB}>Сброс</Button>
+      </div>
+      <!-- <ListItemCell class="width-auto flex-shrink-0 list-input__label-text_color">Заполнение системы маслом:</ListItemCell> -->
+
+<!--       <div slot='title' class="list-input__label-text_color">Внимание! Сброс приведет к установлению заводских настроек</div> -->
+<!--       <div slot='text' class="list-input__label-text_color">      <Button fill small onClick={() => log("Button")}>
+        Сброс
+      </Button></div> -->
+      <!-- <img slot="media" src="static/img/icon.png" width="32" /> -->
+
+
+    </ListItem>
+  </List>
+
+  <BlockTitle>Preloader Button</BlockTitle>
+  <Block strong><Icon icon="icon-info-circled" style="font-size: 28px; color: orange" />
+    <Button fill preloader >Сброс</Button>
+  </Block>
+
+<!--   <Block strong inset class="display-flex">
+    <img src="static/img/icon.png" width="32" />
+    <p>dsfsdgsg</p>
+  </Block> -->
 
   {#each rangeValues[0] as rangeValue}
     <Ranges {...rangeValue} />
@@ -30,16 +81,20 @@
 <script>
     import {
       Page,
+      Button,
       List,
       ListItem,
+      ListItemCell,
       Navbar,
       Block,
       BlockTitle,
       Toggle,
+      Icon,
       useStore
     } from 'framework7-svelte';
     import {t} from '../../services/i18n.js';
     import Ranges from '../../components/range-param.svelte'
+    import log from '../../js/debug.js';
 
     let connected = useStore('connected', (value) => connected = value);
     let system = useStore('system', (value) => system = value);
@@ -53,7 +108,7 @@
         maxValue: 100,
         stepValue: 10,
         scale: false,
-        icon: "icon-drop",
+        icon: "icon-light",
         icon2: "icon-dropfill",
         rangeChange: (e)=>{
           system.bright = e
@@ -62,5 +117,16 @@
         toggle: false,
       }],
     ]
+
+    function signIn() {
+    f7.dialog.alert(`Username: <br>Password: `, () => {
+      f7.loginScreen.close();
+    });
+  }
+
+  function clickB() {
+    log("Button")
+    f7.dialog.confirm('Are you sure that your name is ', () => { f7.dialog.alert('Ok, your name is ');})
+  }
 
 </script>
